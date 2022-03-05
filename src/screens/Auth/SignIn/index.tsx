9b38ed4fc6132c0a5af9113a8bb70ca3cbd1dbcd
Suellen Castro <castrosuellenx@gmail.com>
2022-03-05 11:59:51 -0300

    add initial screens

---
+++ src/screens/Auth/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Title>SignIn</S.Title>
+    </S.Container>
+  );
+};
+
+export default SignIn;
