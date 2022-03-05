106e50845aaad9fed213138ba1d3085843fc0dc1
Suellen Castro <castrosuellenx@gmail.com>
2022-03-05 17:52:05 -0300

    update colors at styles dark theme

--- src/styles/theme/dark.ts
+++ src/styles/theme/dark.ts
@@ -4,7 +4,12 @@ export default {
   title: 'dark',
 
   colors: {
-    text: { primary: '#1F1F1F', secondary: '#A9ABAE' },
+    text: {
+      primary: '#1F1F1F',
+      secondary: '#A9ABAE',
+      blue: '#4866FF',
+      pink: '#FFEEEE',
+    },
 
     background: { primary: '#FAFAFA', secondary: '#4866FF' },
     button: { background: '#4866FF', text: '#FFEEEE' },
