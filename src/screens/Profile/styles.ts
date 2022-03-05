fd82aa35ab1a79f7e15041ac8009a80d7f2ac396
Suellen Castro <castrosuellenx@gmail.com>
2022-03-05 17:51:23 -0300

    update styles screen Profile

--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,7 +1,69 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.colors.background.primary};
 `;
 
-export const Title = styled.Text``;
+export const Content = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const WrapperImage = styled.View`
+  height: ${RFValue(200)}px;
+  width: ${RFValue(200)}px;
+  border-radius: ${RFValue(40)}px;
+  margin-bottom: ${RFValue(30)}px;
+  elevation: 5;
+`;
+
+export const Image = styled.Image.attrs({
+  resizeMode: 'contain',
+})`
+  height: ${RFValue(200)}px;
+  width: ${RFValue(200)}px;
+  border-radius: ${RFValue(35)}px;
+  margin-bottom: ${RFValue(30)}px;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+  font-size: ${RFValue(20)}px;
+  color: ${({ theme }) => theme.colors.text.primary};
+  margin-bottom: ${RFValue(5)}px;
+`;
+
+export const Email = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.regular};
+  font-size: ${RFValue(16)}px;
+  color: ${({ theme }) => theme.colors.text.secondary};
+  margin-bottom: ${RFValue(30)}px;
+`;
+
+export const User = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+  font-size: ${RFValue(16)}px;
+  color: ${({ theme }) => theme.colors.text.blue};
+`;
+
+export const LineUser = styled.View`
+  border-width: ${RFValue(2)}px;
+  border-color: ${({ theme }) => theme.colors.background.secondary};
+  border-radius: ${RFValue(10)}px;
+  padding: ${RFValue(15)}px;
+`;
+
+export const ButtonLogout = styled.TouchableOpacity`
+  align-self: center;
+`;
+
+export const ButtonLogoutText = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.regular};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.text.secondary};
+  text-decoration: underline;
+  margin-bottom: ${RFValue(50)}px;
+`;
