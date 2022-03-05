6f33f2cb75e5c8cd95721d193c15573195e5ff61
Suellen Castro <castrosuellenx@gmail.com>
2022-03-05 16:43:55 -0300

    change screen name Home -> Profile

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import * as S from './styles';
 
-const Home: React.FC = () => {
+const Profile: React.FC = () => {
   return (
     <S.Container>
-      <S.Title>Home</S.Title>
+      <S.Title>Profile</S.Title>
     </S.Container>
   );
 };
 
-export default Home;
+export default Profile;
