97e79b72f660b327a911b3b5e4c603d531e6eb4d
Suellen Castro <castrosuellenx@gmail.com>
2022-03-05 12:45:10 -0300

    update screen SignIn styles

--- src/screens/Auth/SignIn/index.tsx
+++ src/screens/Auth/SignIn/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useTheme } from 'styled-components/native';
 
 import * as S from './styles';
 
 const SignIn: React.FC = () => {
+  const theme = useTheme();
+
   return (
     <S.Container>
+      <S.StatusBar
+        barStyle={theme.colors.barStyle}
+        backgroundColor={theme.colors.background.light}
+      />
       <S.Title>SignIn</S.Title>
     </S.Container>
   );
--- src/screens/Auth/SignIn/styles.ts
+++ src/screens/Auth/SignIn/styles.ts
@@ -1,7 +1,17 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.background.main};
 `;
 
-export const Title = styled.Text``;
+export const StatusBar = styled.StatusBar``;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(25)}px;
+  font-family: ${({ theme }) => theme.fontFamily.regular};
+  color: ${({ theme }) => theme.colors.text.primary};
+`;
